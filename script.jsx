c3db59028b2dc4ffba6a1ca2df9500de9b230b48
Kristen Gunn <ohkristen@yahoo.com>
2016-02-27 12:08:10 -0800

    created a blank card component which will contain a div inside which is an img for avatar, name in a h3, and a horizontal rule after each card.

--- script.jsx
+++ script.jsx
@@ -1,3 +1,15 @@
+var Card = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <img src="" />
+        <h3>Name Here</h3>
+        <hr/>
+        </div>
+    )
+  }
+})
+
 var Main = React.createClass({
   render: function () {
     return (
